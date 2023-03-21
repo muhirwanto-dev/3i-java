48a20d5d5528e50df45aa8f87c4a3e9c771d009f
Muhammad Irwanto <muhammad.irwanto95@gmail.com>
2023-03-21 17:16:12 +0700

    feat: add home page & end point to open module(s)

--- src/src/main/java/com/muhirwanto/ijava/src/SrcApplication.java
+++ src/src/main/java/com/muhirwanto/ijava/src/SrcApplication.java
@@ -2,12 +2,22 @@ package com.muhirwanto.ijava.src;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SrcApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(SrcApplication.class, args);
 	}
 
+	@GetMapping("module")
+	public String OpenModule(@RequestParam(name = "name", defaultValue = "1") int index)
+	{
+		return String.format("Module %d opened!", index);
+	}
 }
